2ebdfc3c59503de6cc53cc01c6047defbdf68ea8
Lucas-Schwengber <lucaschwengber@hotmail.com>
2024-08-22 11:29:24 -0700

    Improve spacing

--- newton.py
+++ newton.py
@@ -1,13 +1,16 @@
 import numpy as np
 
+
 def derivative(f, eps=1e-6):
     def f_prime(x):
         return (f(x+eps)-f(x))/eps
 
     return f_prime
 
+
 def second_derivative(f, eps=1e-6):
     return derivative(derivative(f))
+
 
 def optimize(f, x0=0, n_iter=100, tol=1e-6):
     f_prime = derivative(f)
@@ -18,15 +21,21 @@ def optimize(f, x0=0, n_iter=100, tol=1e-6):
         if np.abs(f_prime(x)) < tol:
             print("Gradient norm below tolerance, stopping early.")
             break
+        
         if  np.abs(f_prime(x)/f_double_prime(x)) < tol:
             print("Stopping criteria reached.")
             break
+        
         else:
             x = x - f_prime(x)/f_double_prime(x)
             if i == n_iter-1:
                print("Maximum number of iterations reached") 
+        
         print(f"Iteration {i} approximation: {x}") 
+    
     return x
+
+
 
 if __name__ == "__main__":
     f = lambda x: np.sin(x)
