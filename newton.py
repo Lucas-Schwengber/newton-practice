dd1a8294f38964f77894674aed906426cb07e1ff
Lucas-Schwengber <lucaschwengber@hotmail.com>
2024-08-22 13:16:16 -0700

    Lint newton code

--- newton.py
+++ newton.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def derivative(f, eps=1e-6):
     def f_prime(x):
-        return (f(x+eps)-f(x))/eps
+        return (f(x + eps) - f(x)) / eps
 
     return f_prime
 
@@ -17,24 +17,23 @@ def optimize(f, x0=0, n_iter=100, tol=1e-6):
     f_double_prime = second_derivative(f)
     x = x0
 
-    for i in range(0,n_iter):
+    for i in range(0, n_iter):
         if np.abs(f_double_prime(x)) < tol:
             print("Hessian norm below tolerance, stopping early.")
             break
-        
-        if  np.abs(f_prime(x)/f_double_prime(x)) < tol:
+
+        if np.abs(f_prime(x) / f_double_prime(x)) < tol:
             print("Stopping criteria reached.")
             break
-        
-        else:
-            x = x - f_prime(x)/f_double_prime(x)
-            if i == n_iter-1:
-               print("Maximum number of iterations reached") 
-        
-        print(f"Iteration {i} approximation: {x}") 
-    
-    return x
 
+        else:
+            x = x - f_prime(x) / f_double_prime(x)
+            if i == n_iter - 1:
+                print("Maximum number of iterations reached")
+
+        print(f"Iteration {i} approximation: {x}")
+
+    return x
 
 
 if __name__ == "__main__":
