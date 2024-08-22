bb069a95deb2d662c292f7dca8de001af09e0ad5
Lucas-Schwengber <lucaschwengber@hotmail.com>
2024-08-22 13:27:28 -0700

    Add documentation to functions and rename argument

--- newton.py
+++ newton.py
@@ -2,6 +2,17 @@ import numpy as np
 
 
 def derivative(f, eps=1e-6):
+    """
+    Evaluates the derivative of a function (as a function) using finite differences.
+
+    Arguments:
+    -f: the real function to be differenciated
+    -eps: the finite difference approximation step size
+
+    Output:
+    f_prime: a real function which evaluates the finite difference approximation of the derivative of f at a given point
+    """
+
     def f_prime(x):
         return (f(x + eps) - f(x)) / eps
 
@@ -9,15 +20,38 @@ def derivative(f, eps=1e-6):
 
 
 def second_derivative(f, eps=1e-6):
-    return derivative(derivative(f))
+    """
+    Evaluates the second derivative of a given function f (as a function) using finite differences.
+
+    Arguments:
+    -f: the real function to be twice differenciated
+    -eps: the finite difference approximation step size
+
+    Output:
+    f_double_prime: a real function which evaluates the finite difference approximation of the second derivative of f at a given point
+    """
+    f_double_prime = derivative(derivative(f, eps), eps)
+
+    return f_double_prime
 
 
-def optimize(f, x0=0, n_iter=100, tol=1e-6):
-    f_prime = derivative(f)
-    f_double_prime = second_derivative(f)
+def optimize(f, x0=0, max_iter=100, eps=1e-6):
+    """
+    Runs newton's method to find local minima (or maxima) of a given real function f.
+
+    Arguments:
+    -f: the real function to be optimized
+    -x0: starting value
+    -max_iter: maximum number of iterations to run the algorithm
+    -eps: finite difference approximation step size
+    """
+    tol = 1e-6
+
+    f_prime = derivative(f, eps)
+    f_double_prime = second_derivative(f, eps)
     x = x0
 
-    for i in range(0, n_iter):
+    for i in range(0, max_iter):
         if np.abs(f_double_prime(x)) < tol:
             print("Hessian norm below tolerance, stopping early.")
             break
@@ -28,7 +62,7 @@ def optimize(f, x0=0, n_iter=100, tol=1e-6):
 
         else:
             x = x - f_prime(x) / f_double_prime(x)
-            if i == n_iter - 1:
+            if i == max_iter - 1:
                 print("Maximum number of iterations reached")
 
         print(f"Iteration {i} approximation: {x}")
